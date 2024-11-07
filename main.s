92b5fc880ea5e560698c1e21324f50e74a8db79b
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-07 08:31:50 +0000

    print procedure

--- main.s
+++ main.s
@@ -9,14 +9,26 @@ EFI_TEXT_STRING = 8
 entry $
 
   ;;sub     rsp,28h
-  mov rcx,[rdx + EFI_SIMPLE_TEXT_INPUT_PROTOCOL]
+  mov [EFI_SYSTEM_TABLE], rdx
+
+  call print
+  call print
+  call print
+  jmp $
+
+print:
+  mov rdi,[EFI_SYSTEM_TABLE]
+  mov rcx,[rdi + EFI_SIMPLE_TEXT_INPUT_PROTOCOL]
   mov rax,[rcx + EFI_TEXT_STRING]
 
   mov rdx,string
   ;; Set up the shadow space. We just need to reserve 32 bytes
   ;; on the stack, which we do by manipulating the stack pointer:
-  sub rsp, 32
+  sub rsp,32
   call rax
-  jmp $
+  add rsp,32
+  ret
+  
 
 string du 'Fuck C',13,10,0
+EFI_SYSTEM_TABLE dq ?
