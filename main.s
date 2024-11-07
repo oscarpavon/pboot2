36ded913f8bc562656e0150e84bc3eae4e33da31
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-07 08:02:42 +0000

    created function definition

--- main.s
+++ main.s
@@ -1,12 +1,22 @@
 format pe64 efi
 section '.text' code executable readable
 
-entry $
-                sub     rsp,28h
-                mov     rcx,[rdx + 64]
-                mov     rax,[rcx + 8]
-                mov     rdx,string
-                call    rax
-                jmp     $
+;;Calling convertion rcx, rdx, r8, r9 
+EFI_SIMPLE_TEXT_INPUT_PROTOCOL = 64
+EFI_TEXT_STRING = 8
 
-string du 'Hello, World!',13,10,0
+
+entry $
+
+  ;;sub     rsp,28h
+  mov rcx,[rdx + EFI_SIMPLE_TEXT_INPUT_PROTOCOL]
+  mov rax,[rcx + EFI_TEXT_STRING]
+
+  mov rdx,string
+  ;; Set up the shadow space. We just need to reserve 32 bytes
+  ;; on the stack, which we do by manipulating the stack pointer:
+  sub rsp, 32
+  call rax
+  jmp $
+
+string du 'Fuck C',13,10,0
