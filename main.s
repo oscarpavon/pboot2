148ec7093bd968eac8d7f6d1de2f22b70e9768b9
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-10 21:48:02 +0000

    gaming with stack

--- main.s
+++ main.s
@@ -23,8 +23,10 @@ SHADOW_SPACE = 32
 EFI_ALLOCATE_POOL = 5 * 8
 
 entry $
+  push rbp
+  mov rbp,rsp
+  sub     rsp,32
 
-  ;;sub     rsp,28h
   mov [EFI_SYSTEM_TABLE], rdx
   mov [EFI_BOOT_LOADER_HANDLE], rcx
 
@@ -32,7 +34,17 @@ entry $
   call print
 
   call allocate_pool
+  ;call open_protocol
+  mov rax,'asdf'
+  push rax
 
+  mov rdx,string
+  call print
+
+  pop rax
+
+  mov rdx,string
+  call print
   
 main_loop:
 
@@ -46,19 +58,28 @@ error:
 
 
 print:
+  push rbp
+  mov rbp, rsp
+  sub rsp,32
+
   mov rdi,[EFI_SYSTEM_TABLE]
   mov rcx,[rdi + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL]
   mov rax,[rcx + EFI_TEXT_STRING]
 
   ;; Set up the shadow space. We just need to reserve 32 bytes
   ;; on the stack, which we do by manipulating the stack pointer:
-  sub rsp,SHADOW_SPACE
   call rax
-  add rsp,SHADOW_SPACE
+  
+  mov rsp,rbp
+  pop rbp
+  
   ret
 
 allocate_pool:
-  
+  push rbp
+  mov rbp,rsp
+  sub rsp,32
+
   mov r11,[EFI_SYSTEM_TABLE]
   mov r12,[r11 + EFI_BOOT_SERVICES]
   mov r13, [r12 + EFI_ALLOCATE_POOL]
@@ -67,14 +88,18 @@ allocate_pool:
   mov rdx,4;bytes to allocate
   lea r8, [allocated_memory]
 
-  sub rsp, 8*3
   call r13
-  add rsp, 8*3
   cmp rax, EFI_SUCCESS
   jne error
 
+ ; mov rsp, rbp
+ ; pop rbp
+
   mov rdx,memory_allocated_msg
-  call print
+  ;call print
+
+  leave
+ 
 
   ret
 
@@ -86,32 +111,36 @@ open_protocol:
   mov r13, [r12 + EFI_OPEN_PROTOCOL]
 
   mov rcx, EFI_BOOT_LOADER_HANDLE
-  lea rdx, [EFI_LOADED_IMAGE_PROTOCOL_GUID]
+  mov rdx, EFI_LOADED_IMAGE_PROTOCOL_GUID
   lea r8, [bootloader_image]
   mov r9, EFI_BOOT_LOADER_HANDLE
 
 
-
-  sub rsp,SHADOW_SPACE
+  sub rsp,8*6
 
   mov rax, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL
-  push rax
+  mov qword [rsp+8*5],rax
   mov rax, 0
-  push rax
+  mov qword [rsp+8*4],rax
 
   call r13
 
-  add rsp,SHADOW_SPACE
+  add rsp,8*6
 
-  ;cmp rax, EFI_SUCCESS
-  ;jne error
+  cmp rax, EFI_SUCCESS
+  jne error
+
+  mov rdx,open_protocol_ok
+  call print
+  ret
 
 allocated_memory dq ?
 string du 'Fuck C',13,10,0
 error_msg du 'Error open loaded image',13,10,0
 error_memory_msg du 'Error allocating pool',13,10,0
 memory_allocated_msg du 'Allocated pool',13,10,0
+open_protocol_ok du 'Open protocol OK',13,10,0
 EFI_SYSTEM_TABLE dq ?
 EFI_BOOT_LOADER_HANDLE dq ?
-EFI_LOADED_IMAGE_PROTOCOL_GUID dq 0x5b1b31a1, 0x9562, 0x11d2, 0x8e, 0x3f, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b
+EFI_LOADED_IMAGE_PROTOCOL_GUID dd 0x5b1b31a1, 0x956211d2, 0x8e3f00a0 , 0xc969723b
 bootloader_image dq ?
