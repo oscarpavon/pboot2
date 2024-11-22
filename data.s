9274e678a722fec626adb36eb0a0447c5c551b88
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-22 00:53:36 +0000

    data.s cleaned

--- data.s
+++ data.s
@@ -1,23 +1,21 @@
 
+BootLoaderImage dq ?
+KernelImageHandle dq ?
+FileSystemProtocol dq ?
+RootDirectory dq ?
+KernelFile dq ?
+KernelFileSize dq ?
+DevicePathProtocol rq 1
+readed rq 1
+
 boot_services dq ?
 open_protocol dq ?
 
-hex_table db '0123456789abcdef'
-decimal_buffer du '000000000',13,10,0
-hex_buffer db 'FFFFFFFFFFFFFFFF'
-hex_buffer_unicode du '0000000000000000',13,10,0
-decimal du 'A',13,10,0
-
-number dq 10
-
 allocated_memory dq 0
-allocated_memory2 dq 1
-string du 'Fuck C',13,10,0
-all_ok_msg du 'All OK',13,10,0
-
-kernel_not_readed_msg du 'Kernel not readed',13,10,0
 
 kernel_name du 'vmlinuz',0
+
+;messages
 memory_allocated_msg du 'Allocated pool',13,10,0
 open_protocol_ok du 'Open protocol OK',13,10,0
 got_loaded_image du "Got loaded image",13,10,0
@@ -28,6 +26,21 @@ image_loaded du "Image loaded",13,10,0
 setted_max_file du "Setted max file",13,10,0
 file_opened du "File opened",13,10,0
 got_device_path du "Got device path",13,10,0
+
+kernel_not_readed_msg du 'Kernel not readed',13,10,0
+
+all_ok_msg du 'All OK',13,10,0
+
+;error messages
+unsupported_msg du 'Unsupported',13,10,0
+invalid_parameter_msg du 'Invalid Parameter',13,10,0
+not_found_msg du 'Not found',13,10,0
+load_error_msg du 'Load error',13,10,0
+
+error_open_loaded_image_msg du 'Error open loaded image',13,10,0
+error_memory_msg du 'Error allocating pool',13,10,0
+error_msg du 'Error',13,10,0
+error_open_file_msg du 'Error open file',13,10,0
 
 EFI_SYSTEM_TABLE dq ?
 EFI_BOOT_LOADER_HANDLE dq ?
@@ -59,26 +72,9 @@ memory_device_path db EFI_HARDWARE_DEVICE_PATH
                     db 4;size of device path
                     db 0;right 4 siftted 8 bit
 
-BootLoaderImage dq ?
-KernelImageHandle dq ?
-FileSystemProtocol dq ?
-RootDirectory dq ?
-KernelFile dq ?
-KernelFileSize dq ?
-DevicePathProtocol rq 1
-size dd 16483328
-size2 dq 10
-char du 'T'
-readed rq 1
-readed_count rq 1
 
-;error messages
-unsupported_msg du 'Unsupported',13,10,0
-invalid_parameter_msg du 'Invalid Parameter',13,10,0
-not_found_msg du 'Not found',13,10,0
-load_error_msg du 'Load error',13,10,0
-
-error_open_loaded_image_msg du 'Error open loaded image',13,10,0
-error_memory_msg du 'Error allocating pool',13,10,0
-error_msg du 'Error',13,10,0
-error_open_file_msg du 'Error open file',13,10,0
+;debug
+hex_table db '0123456789abcdef'
+decimal_buffer du '000000000',13,10,0
+hex_buffer db 'FFFFFFFFFFFFFFFF'
+hex_buffer_unicode du '0000000000000000',13,10,0
