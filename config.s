53f81b71f1ff658ac97d243e75b370f183aa1bdd
Oscar Pavon <pavon_oscar@outlook.com>
2024-11-27 01:31:57 +0000

    print menu with selected menu sign

--- config.s
+++ config.s
@@ -2,11 +2,13 @@ DEBUG = 1
 
 show_menu db 1
 
-entries db 0;start entries
-  du "Pavon Linux",13,10,0
-  du "Gentoo Linux",13,10,0
-  du "Gentoo2 Linux",13,10,0
-  du "Gentoo3 Linux",13,10,0
+boot_entry db 3
+
+entries du 0;start entries
+  du "Pavon Linux",0
+  du "Gentoo Linux",0
+  du "Gentoo2 Linux",0
+  du "Gentoo3 Linux",0
 db 0xFF;end entries
 
 
--- menu.s
+++ menu.s
@@ -1,38 +1,49 @@
 msg du "end entries",13,10,0
+new_line du 13,10,0
+selected_entry_sign du '*',0
 menu:
 
-  lea r15,[entries+1]
-
-mov r14,r15
-push r14;r14 it's used in print
-mov rdx,r14
-call print_in_menu ;print first entry
-pop r14
+lea r14,[entries]
 xor rcx,rcx
+xor rdi,rdi ;entry number
+xor rsi,rsi
 find_entry:
   push r14
+  mov rsi,rcx
+  find_one_entry:
   add rcx,2
   lea rdx,[r14+rcx]
   cmp word [rdx],0
-  jne find_entry
+  jne find_one_entry
   je print_entry
 
 end_entries:
   mov rdx,msg
   call print
+  
 
   jmp $
 
 print_entry:
-  mov r11,rcx
-  add r11,2
+  inc dil;selected entry counter
+  mov r11,rsi
+  add r11,2;plus end zero
   lea r13,[r14+r11]
   cmp byte [r13],0xFF
   je end_entries
-  push rcx
+  push rcx;print override rcx
   lea rdx,[r14+r11]
+  call print_in_menu
+  cmp dil, [boot_entry]
+  je print_selected_entry_sign
+continue_print_entry:
+  mov rdx,new_line
   call print_in_menu
   pop rcx
   jmp find_entry
 
+print_selected_entry_sign:
+  mov rdx,selected_entry_sign
+  call print_in_menu
+  jmp continue_print_entry
 
